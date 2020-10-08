80e24357b37c4bce82e0a246c95d017c9c7fc73e
wboston <williamboston123@hotmail.com>
2020-10-08 14:03:23 -0700

    Add: event handlers for movement key press

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.Design;
 
 namespace WindowsFormsApp1
 {
@@ -17,5 +18,46 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Escape:
+                    // Pause the Game
+                    // Call Pause Menu overlay
+                    break;
+
+                case Keys.Right:
+                    break;
+                case Keys.Left:
+                    break;
+                case Keys.Up:
+                    break;
+                case Keys.Down:
+                    break;
+
+                case Keys.W:
+                    break;
+                case Keys.S:
+                    break;
+                case Keys.A:
+                    break;
+                case Keys.D:
+                    break;
+
+                case Keys.Space:
+                    // create new projectile
+                    // space shoots from the user.
+                    // get shot(user.x, user.y)
+                    break;
+            }
+
+            // user.doWork(sender, e)
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
     }
 }
