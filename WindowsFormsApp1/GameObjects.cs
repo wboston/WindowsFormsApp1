49b3484d243dfbbaca361c7a16b7de0dce50c258
wboston <williamboston123@hotmail.com>
2020-10-08 12:38:23 -0700

    added the base class for game objects

---
+++ WindowsFormsApp1/GameObjects.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    class GameObjects
+    {
+    }
+
+    class Base
+    {
+        /// <summary>
+        /// General Properties
+        /// </summary>
+        public double Height { get; set; }
+        public double Witdth { get; set; }
+        public Brush Color { get; set; }
+
+
+        /// <summary>
+        /// Coordinates and Movement Speed
+        /// </summary>
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double XSpeed { get; set; }
+        public double YSpeed { get; set; }
+
+    }
+}
