d3fb155dbc2fe8cf7e1335583f6e3a03ce0cbf6c
wboston <williamboston123@hotmail.com>
2020-10-12 18:05:07 -0700

    Update Base.cs
    did some renaming and property protection adjustments. as well as added more descriptions

--- WindowsFormsApp1/Base.cs
+++ WindowsFormsApp1/Base.cs
@@ -27,22 +27,22 @@ namespace WindowsFormsApp1
         /// <summary>
         /// Objects X position
         /// </summary>
-        protected int X { get; set; }
+        private int X { get; set; }
 
         /// <summary>
         /// Objects Y position
         /// </summary>
-        protected int Y { get; set; }
+        private int Y { get; set; }
 
         /// <summary>
         /// Objects X azis movement speed
         /// </summary>
-        protected int XSpeed { get; set; }
+        private int XSpeed { get; set; }
 
         /// <summary>
         /// Object Y axis movement speed
         /// </summary>
-        protected int YSpeed { get; set; }
+        private int YSpeed { get; set; }
 
         /// <summary>
         /// Used to tell if the Object is moving by using -1,0,1 
@@ -73,6 +73,14 @@ namespace WindowsFormsApp1
             MovingY = 0;
         }
 
+        /// <summary>
+        /// Not the default constructor used for manually setting the objects attributes
+        /// </summary>
+        /// <param name="height"> sets objects Height dimension </param>
+        /// <param name="width"> sets objects width diension </param>
+        /// <param name="color"> sets objects color </param>
+        /// <param name="x_speed"> sets the x speed of the object</param>
+        /// <param name="y_speed"> sets the y speed of the object</param>
         public Base(int height, int width, Brush color, int x_speed, int y_speed)
         {
             // dimension and color
@@ -99,9 +107,14 @@ namespace WindowsFormsApp1
         }
 
         /// <summary>
-        /// Abstract Member functions for controlling the object
+        /// Function responsible for moving the object bc position is a private attribute
         /// </summary>
         abstract public void MoveX(int x);//movement is multiplied by int to get up down left right movemet
+        
+        /// <summary>
+        /// Function responsible for moving the object bc position if a private attribute
+        /// </summary>
+        /// <param name="y"></param>
         abstract public void MoveY(int y);
 
         abstract public bool StopMoveX(int x);
